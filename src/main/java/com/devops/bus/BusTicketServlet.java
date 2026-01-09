7a1c97ce5ede66d00b5acd11bb09b9d35ad6034e
tejesh <tejeshthej9100@gmail.com>
2026-01-09 14:20:57 +0530

    adding war file

---
+++ src/main/java/com/devops/bus/BusTicketServlet.java
@@ -0,0 +1,57 @@
+package com.devops.bus;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/book")
+public class BusTicketServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        response.setContentType("text/html");
+
+        String busName = "KSRTC Express";
+        int ticketPrice = 500;
+
+        response.getWriter().println("<html><body>");
+        response.getWriter().println("<h2>Bus Ticket Booking</h2>");
+
+        response.getWriter().println("<form method='post'>");
+        response.getWriter().println("Passenger Name: <input type='text' name='name'/><br><br>");
+        response.getWriter().println("Number of Seats: <input type='number' name='seats'/><br><br>");
+        response.getWriter().println("<input type='submit' value='Book Ticket'/>");
+        response.getWriter().println("</form>");
+
+        response.getWriter().println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        String name = request.getParameter("name");
+        int seats = Integer.parseInt(request.getParameter("seats"));
+
+        String busName = "KSRTC Express";
+        int ticketPrice = 500;
+        int totalAmount = seats * ticketPrice;
+
+        response.setContentType("text/html");
+        response.getWriter().println("<html><body>");
+
+        response.getWriter().println("<h2>Booking Confirmed</h2>");
+        response.getWriter().println("Passenger Name : " + name + "<br>");
+        response.getWriter().println("Bus Name : " + busName + "<br>");
+        response.getWriter().println("Seats Booked : " + seats + "<br>");
+        response.getWriter().println("Total Amount : ₹" + totalAmount + "<br>");
+        response.getWriter().println("<b>Status : CONFIRMED</b>");
+
+        response.getWriter().println("</body></html>");
+    }
+}
+
